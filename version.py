1158c7b6d56d6b25be66b598323eaf1a609c23d4
dongbohu <dongbo.hu@gmail.com>
2020-12-26 15:29:30 -0500

    Merge functions in version.py together into one

--- version.py
+++ version.py
@@ -1,51 +1,5 @@
 #!/usr/bin/env python3
 
-import requests
-
-def get_url_content(url):
-    """Request an input URL and return text lines in the reponse."""
-
-    resp = requests.get(url)
-    text_lines = resp.text.strip('\n').split('\n')
-    return text_lines
-
-
-def get_obo_release():
-    """Parse HumanDO.obo data file and return its release date."""
-
-    url = "https://raw.githubusercontent.com/DiseaseOntology/HumanDiseaseOntology/main/src/ontology/HumanDO.obo"
-    text_lines = get_url_content(url)
-    release_date = ""
-
-    # Find the line that is in the following format:
-    # "data-version: doid/releases/YYYY-MM-DD/doid-non-classified.obo"
-    # and extract "YYYY-MM-DD" part as the release string.
-    for line in text_lines:
-        if line.startswith("data-version: "):
-            full_version = line.strip().split(' ')[1]
-            release_date = full_version.split('/')[2]
-            break
-
-    return release_date
-
-
-def get_genemap2_release():
-    """Parse genemap2.txt data file and return its release date."""
-
-    url = "https://raw.githubusercontent.com/greenelab/disease_ontology_geneset/master/data/latest/genemap2.txt"
-    text_lines = get_url_content(url)
-    genemap2_release = ""
-
-    # Find the line that is in the following format:
-    # "data-version: doid/releases/YYYY-MM-DD/doid-non-classified.obo"
-    # and extract "YYYY-MM-DD" part as the release string.
-    for line in text_lines:
-        if line.startswith("# Generated: "):
-            release_date = line.strip().split(': ')[1]
-            break
-
-    return release_date
-
 
 def get_release(self):
     """
@@ -53,8 +7,38 @@ def get_release(self):
     genemap2 data files.
     """
 
-    obo_release = get_obo_release()
-    genemap2_release = get_genemap2_release()
+    import requests
+
+    # Get release date of "HumanDO.obo" data file:
+    obo_url = "https://raw.githubusercontent.com/DiseaseOntology/HumanDiseaseOntology/main/src/ontology/HumanDO.obo"
+    obo_resp = requests.get(obo_url)
+    obo_text_lines = obo_resp.text.strip('\n').split('\n')
+    obo_release = ""
+
+    # Find the line that is in the following format:
+    # "data-version: doid/releases/YYYY-MM-DD/doid-non-classified.obo"
+    # and extract "YYYY-MM-DD" part as the release string.
+    for line in obo_text_lines:
+        if line.startswith("data-version: "):
+            full_version = line.strip().split(' ')[1]
+            obo_release = full_version.split('/')[2]
+            break
+
+    # Get release date of "genemap2.txt" data file:
+    genemap2_url = "https://raw.githubusercontent.com/greenelab/disease_ontology_geneset/master/data/latest/genemap2.txt"
+    genemap2_resp = requests.get(genemap2_url)
+    genemap2_text_lines = genemap2_resp.text.strip('\n').split('\n')
+    genemap2_release = ""
+
+    # Find the line that is in the following format:
+    # "# Genearated: YYYY-MM-DD"
+    # and extract "YYYY-MM-DD" part as the release string.
+    for line in genemap2_text_lines:
+        if line.startswith("# Generated: "):
+            genemap2_release = line.strip().split(': ')[1]
+            break
+
+    # Return a string that combines both release dates
     return "obo-" + obo_release + "_" + "genemap2-" + genemap2_release
 
 
